d20a83d055117113b5a52606c7596ca174aa90b9
Nada Ika Sari <nadaikasari01@gmail.com>
2025-04-10 00:40:02 +0700

    fix : delete unused code

--- src/app/loan/dto/loan.dto.ts
+++ src/app/loan/dto/loan.dto.ts
@@ -2,12 +2,3 @@ export class BookLoanDto {
   member_code: string;
   book_code: string;
 }
-
-export class ResultCreateLoan {
-  member_id: number;
-  book_id: number;
-  borrowed_at: Date;
-  due_date: Date;
-  returned_at: string;
-  stock: number;
-}
