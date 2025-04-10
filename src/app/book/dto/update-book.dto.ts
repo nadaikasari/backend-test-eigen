b9d490eca514873ba2d0c01608975af757d9ee97
Nada Ika Sari <nadaikasari01@gmail.com>
2025-04-10 12:52:17 +0700

    feat : add api property in book dto

--- src/app/book/dto/update-book.dto.ts
+++ src/app/book/dto/update-book.dto.ts
@@ -1,16 +1,29 @@
 import { IsOptional, IsString, IsInt, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateBookDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: 'The title of the book',
+    example: 'Laut Bercerita',
+  })
   title?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: 'The author of the book',
+    example: 'Leila S. Chudori',
+  })
   author?: string;
 
   @IsOptional()
   @IsInt()
   @Min(0)
+  @ApiProperty({
+    description: 'Stock book',
+    example: 1,
+  })
   stock?: number;
 }
