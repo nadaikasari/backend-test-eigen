9d6b253dc20b92ca3f00c210d35cf8d9859a67d7
Nada Ika Sari <nadaikasari01@gmail.com>
2025-04-10 11:21:15 +0700

    fix : add description for api

--- src/app/book/dto/book.dto.ts
+++ src/app/book/dto/book.dto.ts
@@ -1,4 +1,5 @@
 import { IsString, IsInt, IsNotEmpty, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BookDto {
   @IsString()
@@ -7,13 +8,25 @@ export class BookDto {
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The title of the book',
+    example: 'Laut Bercerita',
+  })
   title: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The author of the book',
+    example: 'Leila S. Chudori',
+  })
   author: string;
 
   @IsInt()
   @Min(0)
+  @ApiProperty({
+    description: 'Stock book',
+    example: 1,
+  })
   stock: number;
 }
--- src/app/loan/dto/loan.dto.ts
+++ src/app/loan/dto/loan.dto.ts
@@ -1,4 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString} from 'class-validator';
+
 export class BookLoanDto {
+  @IsString()
+  @ApiProperty({
+    description: 'User Member Code',
+    example: 'MBR001',
+  })
   member_code: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Book Code',
+    example: 'BK001',
+  })
   book_code: string;
 }
